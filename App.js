836e292d34621628b3d7e102dd78a5be12375171
yuslianto <yuslianto56@gmail.com>
2019-01-08 21:10:17 +0700

    adding event in form data

--- App.js
+++ App.js
@@ -23,7 +23,7 @@ class App extends Component {
       ],
       formData:{
         nama: '',
-        jk: '',
+        jk: 0,
         tgl_lahir: null,
         email: '',
         telp: '',
@@ -32,7 +32,7 @@ class App extends Component {
     }
   }
   render() {
-
+    const {nama, jk, tgl_lahir, email, telp, pekerjaan} = this.state.formData;
     return (
       <ScrollView>
         <KeyboardAvoidingView style={styles.container} enabled>
@@ -41,8 +41,15 @@ class App extends Component {
             <TextInput
               style={styles.txtInput}
               underlineColorAndroid="blue"
-              //onChangeText={(text) => this.setState({text})}
-              //value={this.state.text}
+              onChangeText={nama => 
+                this.setState(prevState =>({
+                  formData: {
+                    ...prevState.formData,
+                    nama
+                  }
+                }))
+              }
+              value={nama}
             />
           </View>
           <View style={styles.row}>
@@ -55,19 +62,26 @@ class App extends Component {
               labelStyle={{paddingRight: 20}}
               buttonColor={'#2196f3'}
               animation={true}
-              //onPress={(value) => {this.setState({value:value})}}
+              onPress={jk => 
+                this.setState(prevState =>({
+                  formData: {
+                    ...prevState.formData,
+                    jk
+                  }
+                }))
+              }
             />
           </View>
           <View style={styles.row}>
             <Text style={[styles.label, {marginBottom:10}]}>Tanggal Lahir</Text>
             <DatePicker
               style={{width: "100%"}}
-              //date={this.state.date}
+              date={tgl_lahir}
               mode="date"
               placeholder="select date"
               format="DD-MM-YYYY"
-              minDate="1909-05-01"
-              maxDate="2016-06-01"
+              minDate="19-05-1501"
+              maxDate="20-06-3011"
               confirmBtnText="Confirm"
               cancelBtnText="Cancel"
               customStyles={{
@@ -77,12 +91,15 @@ class App extends Component {
                   top: 4,
                   marginLeft: 0
                 },
-                dateInput: {
-                  //marginLeft: 36
-                }
-                // ... You can check the source to find the other keys.
               }}
-              //onDateChange={(date) => {this.setState({date: date})}}
+              onDateChange={(tgl_lahir) => 
+                this.setState(prevState => ({
+                  formData: {
+                    ...prevState.formData,
+                    tgl_lahir
+                  }
+                }))
+              }
             />
           </View>
           <View style={styles.row}>
@@ -91,8 +108,15 @@ class App extends Component {
               style={styles.txtInput}
               underlineColorAndroid="blue"
               keyboardType="email-address"
-              //onChangeText={(text) => this.setState({text})}
-              //value={this.state.text}
+              onChangeText={email => 
+                this.setState(prevState =>({
+                  formData: {
+                    ...prevState.formData,
+                    email
+                  }
+                }))
+              }
+              value={email}
             />
           </View>
           <View style={styles.row}>
@@ -101,24 +125,43 @@ class App extends Component {
               style={styles.txtInput}
               underlineColorAndroid="blue"
               keyboardType="phone-pad"
-              //onChangeText={(text) => this.setState({text})}
-              //value={this.state.text}
+              onChangeText={telp => 
+                this.setState(prevState =>({
+                  formData: {
+                    ...prevState.formData,
+                    telp
+                  }
+                }))
+              }
+              value={telp}
             />
           </View>
           <View style={styles.row}>
             <Text style={styles.label}>Pekerjaan</Text>
             <Picker
-              //selectedValue={this.state.language}
+              selectedValue={pekerjaan}
               style={{ height: 50}}
-              //onValueChange={(itemValue, itemIndex) => this.setState({language: itemValue})}
+              onValueChange={pekerjaan => 
+                this.setState(prevState => ({
+                  formData: {
+                    ...prevState.formData,
+                    pekerjaan
+                  }
+                }))
+              }
             >
             {
               this.state.dataPekerjaan.map((item, index)=>(
-                <Picker.Item key={index} label={item} value={item.toLocaleLowerCase()}/>
+                <Picker.Item 
+                  key={index}
+                  label={item} 
+                  value={item.toLocaleLowerCase()}
+                  />
               ))
             }
             </Picker>
           </View>
+          <Text style={styles.label}>{JSON.stringify(this.state.formData)}</Text>
           <View style={styles.row}>
             <TouchableHighlight 
               style={styles.btnContainer}
